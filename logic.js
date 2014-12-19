fe17d69a697fb469886aa32fd1184ef101a48c33
Sourcebits-Nagendra <nagendra.rao@sourcebits.com>
2014-12-19 12:35:32 +0530

    Update logic.js

--- logic.js
+++ logic.js
@@ -1,1 +1,11 @@
-define()
+define(function(require,exports,module){
+  var logic = function(){
+    var g = document.getElementsByTagName("input")[0];
+    g.addEventListener("change", function(){
+      if (isNAN(g.value)){
+        alert("Enter digits only");
+      }
+    })
+  };
+  return logic;
+});
